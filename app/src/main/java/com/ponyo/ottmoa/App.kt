570d7edee4d1ec92bd2bd48509619b3a4c72b2a1
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-05-21 03:23:55 +0900

    모바일 디버깅 툴 flipper 적용

--- app/src/main/java/com/ponyo/ottmoa/App.kt
+++ app/src/main/java/com/ponyo/ottmoa/App.kt
@@ -1,8 +1,26 @@
 package com.ponyo.ottmoa
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+
+            // 추가로 사용할 플러그인은 여기에서 추가
+
+            client.start()
+        }
+    }
 }
