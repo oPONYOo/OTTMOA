29d4d2652b9b0f8e1185aa3664b121cbd80a5269
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-05-23 16:16:33 +0900

    okhttp 적용

--- data/src/main/java/com/ponyo/data/di/NetworkModule.kt
+++ data/src/main/java/com/ponyo/data/di/NetworkModule.kt
@@ -1,10 +1,20 @@
 package com.ponyo.data.di
 
+
+import android.content.Context
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.ponyo.data.YoutubeApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Qualifier
@@ -16,15 +26,40 @@ annotation class ApiService
 @InstallIn(SingletonComponent::class)
 @Module
 class NetworkModule {
-    private fun provideRetrofit(): Retrofit = Retrofit.Builder()
+
+
+
+    fun initFlipper(@ApplicationContext appContext: Context) {
+        SoLoader.init(appContext, false)
+
+        val client = AndroidFlipperClient.getInstance(appContext)
+        client.addPlugin(InspectorFlipperPlugin(appContext, DescriptorMapping.withDefaults()))
+        client.addPlugin(networkFlipperPlugin)
+        client.start()
+
+
+    }
+
+
+    private val networkFlipperPlugin = NetworkFlipperPlugin()
+    private val flipperOkHttpInterceptor = FlipperOkhttpInterceptor(networkFlipperPlugin)
+
+    private fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient()
+            .newBuilder()
+            .addInterceptor(flipperOkHttpInterceptor)
+            .build()
+
+    private fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl("https://www.googleapis.com/youtube/v3/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
         .build()
 
 
     @ApiService
     @Provides
     @Singleton
-    fun provideApi(): YoutubeApi = provideRetrofit()
+    fun provideApi(): YoutubeApi = provideRetrofit(provideOkHttpClient())
         .create(YoutubeApi::class.java)
 }
